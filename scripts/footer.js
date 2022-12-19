b6d602ec659b15f85b185b50d78b53791ce5aad2
Malekai <>
2022-12-19 18:24:51 +0000

    gh pages: sync 'main'

--- scripts/footer.js
+++
@@ -1,5 +0,0 @@
-var span = document.getElementById("current-year");
-if (span) {
-  var year = (new Date()).getFullYear().toString();
-  span.innerHTML = year;
-}
--- scripts/lib.js
+++
@@ -1,9 +0,0 @@
-function $(selector, context) {
-  var result = [];
-  for (const match of ((context || document).querySelectorAll(selector) || [])) {
-    result.push(match);
-  }
-  return result;
-}
-
-window.$ = $;
